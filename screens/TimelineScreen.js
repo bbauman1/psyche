fb1f8187f3c70fa2b7206830045e9b57ae2861c2
zeusmedina <zeusmedina2@gmail.com>
2018-01-22 21:08:00 -0700

    started timeline

--- screens/TimelineScreen.js
+++ screens/TimelineScreen.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { LinearGradient } from 'expo';
+import Timeline from 'react-native-timeline-listview'
 
 export default class TimelineScreen extends React.Component {
+  constructor() {
+    super()
+    this.data = [
+      { time: '2022', title: 'Launch', description: 'Launch of Psyche spacecraft from NASA’s Kennedy Space Center, FL' },
+      { time: '2023', title: 'Mars', description: 'Psyche spacecraft flyby of Mars' },
+      { time: '2026', title: 'Reaches Orbit', description: 'Psyche spacecraft arrives in asteroid’s orbit' },
+      { time: '2027', title: 'Gathers Data From Orbit', description: 'Psyche spacecraft orbits the Psyche asteroid for 21 months' }
+    ]
+  }
+
   static navigationOptions = {
     title: 'Timeline',
   };
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        
-      </ScrollView>
+      <View style={styles.container}>
+        <Timeline
+          style={styles.list}
+          data={this.data}
+        />
+      </View>
     );
   }
 }
@@ -20,5 +35,9 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 15,
     backgroundColor: '#fff',
+  },
+  list: {
+    flex: 1,
+    marginTop: 20,
   },
 });
