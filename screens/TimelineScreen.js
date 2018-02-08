dc1b82f29329c3cc32f1e2ec0beef06c5253723c
Zeus <zeusmedina@users.noreply.github.com>
2018-02-08 16:46:59 -0700

    Timeline boilerplate list and card component created

--- screens/TimelineScreen.js
+++ screens/TimelineScreen.js
@@ -1,20 +1,27 @@
-import React from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default class TimelineScreen extends React.Component {
+  constructor() {
+    super()
+  }
+
   static navigationOptions = {
     title: "Timeline"
   };
 
   render() {
-    return <ScrollView style={styles.container} />;
+    return (
+      <View style={styles.container}>
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: "#fff"
-  }
+    paddingTop: 0,
+    backgroundColor: "#FFFFFF",
+  },
 });
