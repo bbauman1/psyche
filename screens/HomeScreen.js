bdd975e21dde40f56e5bcb06b0977a7b2606ad24
Brett Bauman <bauman.brett3@gmail.com>
2018-02-07 19:38:14 -0700

    added camera button with link to modal

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,22 @@
 import React from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, Button } from "react-native";
+import { StackNavigator } from "react-navigation";
+import { Ionicons } from "@expo/vector-icons";
 
 export default class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: "Countdown"
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: "Countdown",
+      headerLeft: (
+        <Ionicons
+          name={"ios-camera-outline"}
+          size={32}
+          color={"#000"}
+          style={{ marginLeft: 18 }}
+          onPress={() => navigation.navigate("Modal")}
+        />
+      )
+    };
   };
 
   render() {
