7f218074338f75597b049a1080e1b29b965b603d
Thomas F <taforres@asu.edu>
2018-02-08 13:19:58 -0700

    Count down clock w/ interval refresh to main screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,17 +1,63 @@
 import React from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, View, Text } from "react-native";
+import timeTillLaunch from "../util/countdown";
+import launchDate from "../constants/Dates";
 
 export default class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.set;
+  }
+
+  componentDidMount() {
+    setInterval(
+      () =>
+        this.setState(() => {
+          return {
+            countdown: this._get_current_countdown()
+          };
+        }),
+      1000
+    );
+  }
   static navigationOptions = {
     title: "Countdown"
   };
 
+  _get_current_countdown() {
+    return timeTillLaunch.timeTillLaunch(
+      new Date().getTime(),
+      "2022-08-20T06:59:00Z"
+    );
+  }
+
   render() {
-    return <ScrollView style={styles.container} />;
+    let countdown = this._get_current_countdown();
+    return (
+      <ScrollView style={styles.container}>
+        <View style={styles.countdownContainer}>
+          <Text style={styles.countdownText}>{countdown.years} Ys</Text>
+          <Text style={styles.countdownText}>{countdown.days} Ds</Text>
+          <Text style={styles.countdownText}>{countdown.hours} Hs</Text>
+          <Text style={styles.countdownText}>{countdown.minutes} Ms</Text>
+          <Text style={styles.countdownText}>{countdown.seconds} Ss</Text>
+        </View>
+      </ScrollView>
+    );
   }
 }
 
 const styles = StyleSheet.create({
+  countdownText: {
+    fontWeight: "bold",
+    color: "purple"
+  },
+  countdownContainer: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  },
   container: {
     flex: 1,
     paddingTop: 15,
