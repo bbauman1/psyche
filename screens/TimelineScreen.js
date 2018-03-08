672f67e100460c6aee4dae496abd48540082a645
zeusmedina <zeusmedina2@gmail.com>
2018-03-07 20:41:31 -0700

    All timeline cards filled using json date, tapping leads to detail screen

--- screens/TimelineScreen.js
+++ screens/TimelineScreen.js
@@ -12,6 +12,11 @@ import { StackNavigator } from "react-navigation";
 
 // Import timeline string data stored as JSON
 var PHASE_A = require("../timeline_json/PHASE_A.json");
+var PHASE_B = require("../timeline_json/PHASE_B.json");
+var PHASE_C = require("../timeline_json/PHASE_C.json");
+var PHASE_D = require("../timeline_json/PHASE_D.json");
+var PHASE_E = require("../timeline_json/PHASE_E.json");
+var PHASE_F = require("../timeline_json/PHASE_F.json");
 
 class Timeline extends React.Component {
   static navigationOptions = {
@@ -28,27 +33,93 @@ class Timeline extends React.Component {
           <TouchableWithoutFeedback
             onPress={() => this.props.navigation.navigate("Details")}
           >
+            {/* PHASE A CARD */}
             <View style={styles.card}>
               <LinearGradient
                 colors={["#4c669f", "#3b5998", "#192f6a"]}
                 style={styles.gradient}
               >
-                <Text style={styles.phaseText}>Phase A</Text>
-                <Text style={styles.titleText}>Concept Study</Text>
-                <Text style={styles.dateText}>Sept 2015 - Dec 2016</Text>
+                <Text style={styles.phaseText}>{PHASE_A.phase}</Text>
+                <Text style={styles.titleText}>{PHASE_A.title}</Text>
+                <Text style={styles.dateText}>{PHASE_A.date}</Text>
               </LinearGradient>
             </View>
           </TouchableWithoutFeedback>
-          <View style={styles.card}>
-            <LinearGradient
-              colors={["#4c669f", "#3b5998", "#192f6a"]}
-              style={styles.gradient}
-            >
-              <Text style={styles.phaseText}>Phase B</Text>
-              <Text style={styles.titleText}>Concept Study</Text>
-              <Text style={styles.dateText}>Sept 2015 - Dec 2016</Text>
-            </LinearGradient>
-          </View>
+          <TouchableWithoutFeedback
+            onPress={() => this.props.navigation.navigate("Details")}
+          >
+            {/* PHASE B CARD */}
+            <View style={styles.card}>
+              <LinearGradient
+                colors={["#4c669f", "#3b5998", "#192f6a"]}
+                style={styles.gradient}
+              >
+                <Text style={styles.phaseText}>{PHASE_B.phase}</Text>
+                <Text style={styles.titleText}>{PHASE_B.title}</Text>
+                <Text style={styles.dateText}>{PHASE_B.date}</Text>
+              </LinearGradient>
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback
+            onPress={() => this.props.navigation.navigate("Details")}
+          >
+            {/* PHASE C CARD */}
+            <View style={styles.card}>
+              <LinearGradient
+                colors={["#4c669f", "#3b5998", "#192f6a"]}
+                style={styles.gradient}
+              >
+                <Text style={styles.phaseText}>{PHASE_C.phase}</Text>
+                <Text style={styles.titleText}>{PHASE_C.title}</Text>
+                <Text style={styles.dateText}>{PHASE_C.date}</Text>
+              </LinearGradient>
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback
+            onPress={() => this.props.navigation.navigate("Details")}
+          >
+            {/* PHASE D CARD */}
+            <View style={styles.card}>
+              <LinearGradient
+                colors={["#4c669f", "#3b5998", "#192f6a"]}
+                style={styles.gradient}
+              >
+                <Text style={styles.phaseText}>{PHASE_D.phase}</Text>
+                <Text style={styles.titleText}>{PHASE_D.title}</Text>
+                <Text style={styles.dateText}>{PHASE_D.date}</Text>
+              </LinearGradient>
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback
+            onPress={() => this.props.navigation.navigate("Details")}
+          >
+            {/* PHASE E CARD */}
+            <View style={styles.card}>
+              <LinearGradient
+                colors={["#4c669f", "#3b5998", "#192f6a"]}
+                style={styles.gradient}
+              >
+                <Text style={styles.phaseText}>{PHASE_E.phase}</Text>
+                <Text style={styles.titleText}>{PHASE_E.title}</Text>
+                <Text style={styles.dateText}>{PHASE_E.date}</Text>
+              </LinearGradient>
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback
+            onPress={() => this.props.navigation.navigate("Details")}
+          >
+            {/* PHASE F CARD */}
+            <View style={styles.card}>
+              <LinearGradient
+                colors={["#4c669f", "#3b5998", "#192f6a"]}
+                style={styles.gradient}
+              >
+                <Text style={styles.phaseText}>{PHASE_F.phase}</Text>
+                <Text style={styles.titleText}>{PHASE_F.title}</Text>
+                <Text style={styles.dateText}>{PHASE_F.date}</Text>
+              </LinearGradient>
+            </View>
+          </TouchableWithoutFeedback>
         </ScrollView>
       </View>
     );
@@ -61,7 +132,7 @@ class DetailsScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Details Screen</Text>
+        <ScrollView />
       </View>
     );
   }
@@ -99,7 +170,7 @@ const styles = StyleSheet.create({
   titleText: {
     color: "#FFFFFF",
     marginTop: 10,
-    fontSize: 26
+    fontSize: 18
   },
   dateText: {
     color: "#FFFFFF",
