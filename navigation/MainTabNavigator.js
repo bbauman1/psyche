0bd7cf6fc15fb12bcda585d935db449abe43783e
Brett Bauman <bbauman1@asu.edu>
2018-03-30 13:26:33 -0700

    Horizontal Countdown Clocks (#28)
    * Add different clock
    
    * Add vertical clock
    
    * Add seperate dates for clocks
    
    * switching button
    
    * can switch clocks now
    
    * clear interval when unmounting
    
    * rm old icon
    
    * improved centering on horizontal clock screen
    
    * fixed css warning

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -50,8 +50,8 @@ export default TabNavigator(
             break;
           case "Home":
             let homeStyle = Platform.OS === "ios"
-              ? { height: 48, width: 48, marginBottom: 10, backgroundColor: "#fff" }
-              : { height: 32, width: 32, marginBottom: -1, backgroundColor: "#fff" };
+              ? { height: 48, width: 48, marginBottom: 10 }
+              : { height: 32, width: 32, marginBottom: -1 };
             return (
               <Image
                 source={require("../assets/images/psyche-icon.png")}
