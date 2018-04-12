23773d21118b083b1cf92025d4b31b8ddfbedb1e
Brett Bauman <bbauman1@asu.edu>
2018-04-11 22:32:41 -0700

    Tabicons (#51)
    * .
    
    * add new tab icons

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -37,16 +37,18 @@ export default TabNavigator(
         let iconName;
         switch (routeName) {
           case "Facts":
-            iconName =
-              Platform.OS === "ios"
-                ? `ios-information-circle${focused ? "" : "-outline"}`
-                : "md-information-circle";
+            return (
+              <Image
+                source={require("../assets/images/fact.png")}
+                style={{ height: 28, width: 28, resizeMode: "contain" }} />
+            );
             break;
           case "Timeline":
-            iconName =
-              Platform.OS === "ios"
-                ? `ios-time${focused ? "" : "-outline"}`
-                : "md-time";
+            return (
+              <Image
+                source={require("../assets/images/timeline.png")}
+                style={{ height: 28, width: 28, resizeMode: "contain" }} />
+            );
             break;
           case "Home":
             let homeStyle = Platform.OS === "ios"
@@ -59,16 +61,18 @@ export default TabNavigator(
             );
             break;
           case "Social":
-            iconName =
-              Platform.OS === "ios"
-                ? `ios-chatbubbles${focused ? "" : "-outline"}`
-                : "md-chatbubbles";
+            return (
+              <Image
+                source={require("../assets/images/social.png")}
+                style={{ height: 28, width: 28, resizeMode: "contain" }} />
+            );
             break;
           case "Gallery":
-            iconName =
-              Platform.OS === "ios"
-                ? `ios-images${focused ? "" : "-outline"}`
-                : "md-images";
+            return (
+              <Image
+                source={require("../assets/images/gallery.png")}
+                style={{ height: 28, width: 28, resizeMode: "contain" }} />
+            );
             break;
         }
         return (
